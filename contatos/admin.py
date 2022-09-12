a6726cf75613ecc8143f693f3afbf4d78b0fece4
francisval <francisvalcs@gmail.com>
2022-09-12 16:07:43 -0300

    Obtendo dados do banco e exibindo na pagina principal

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome','sobrenome','email')
+    list_display = ('id','nome','sobrenome','email','categoria')
     list_display_links = ('id','nome','sobrenome')
-    list_filter = ('nome','sobrenome')
+    #list_filter = ('nome','sobrenome')
     list_per_page = 10
     search_fields = ('nome','sobrenome','telefone')
 
